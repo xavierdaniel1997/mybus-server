ef32f10b811a57d8e243c4137a069cd973f35910
Daniel Xavier <danielcx532@gmail.com>
2025-10-10 21:49:31 +0530

    create login, registration and logout api's

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { registerUser } from '../controller/authController';
+import { registerUser, registerVerifiedUser, resendOtp, verifyOtpAndUser } from '../controller/authController';
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/verify-otp", verifyOtpAndUser);
+router.post("/resend-otp", resendOtp);
+router.put("/register-verified-user", registerVerifiedUser)
 
 export default router;

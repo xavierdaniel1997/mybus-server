64e2f2e5889809b33b520273de8e72a4ee4d1ca3
Daniel Xavier <danielcx532@gmail.com>
2025-10-11 22:02:55 +0530

    create the api for refresh access token

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, registerUser, registerVerifiedUser, resendOtp, verifyOtpAndUser } from '../controller/authController';
+import { loginUser, logoutUser, refreshAccessToken, registerUser, registerVerifiedUser, resendOtp, verifyOtpAndUser } from '../controller/authController';
 
 const router = express.Router();
 
@@ -7,6 +7,8 @@ router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtpAndUser);
 router.post("/resend-otp", resendOtp);
 router.put("/register-verified-user", registerVerifiedUser);
-router.post("/login-user", loginUser)
+router.post("/login-user", loginUser),
+router.post("/logout-user", logoutUser),
+router.post("/new-accesstoken", refreshAccessToken)
 
 export default router;

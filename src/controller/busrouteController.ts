a5afbd4784f3598f7417b134bfff185bde9f9259
Daniel Xavier <danielcx532@gmail.com>
2025-10-23 21:59:56 +0530

    fix the issue in busroute

--- src/controller/busrouteController.ts
+++ src/controller/busrouteController.ts
@@ -4,14 +4,14 @@ import { createBusRouteService } from "../service/routeService";
 const createRouteController  = async (req: Request, res: Response) => {
     try{
          const route = await createBusRouteService(req.body);
-    res.status(201).json({
-      success: true,
+    res.status(200).json({
+      success: true,    
       message: "Bus route created successfully",
       data: route,
     });
     }catch(error){
-        res.status(400).json({message: "Failed to create a new route"})
+        res.status(400).json({message: "Failed to create a new route", error})
     }
 }
 
-export {createRouteController}
+export {createRouteController} 

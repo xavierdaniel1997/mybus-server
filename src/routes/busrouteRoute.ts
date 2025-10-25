709c44ea39cc9d3d022e174d141a1a34f1055956
Daniel Xavier <danielcx532@gmail.com>
2025-10-25 20:18:11 +0530

    create the api for get the busroutes

--- src/routes/busrouteRoute.ts
+++ src/routes/busrouteRoute.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { upload } from '../middleware/multer';
 import { isAdmin, isAuth } from '../middleware/isAuth';
-import { createRouteController, updateRouteController } from '../controller/busrouteController';
+import { createRouteController, getBusRouteByIdController, updateRouteController } from '../controller/busrouteController';
 ;
 
 const router = express.Router();
 
 router.post("/create-new-busroute", isAuth, isAdmin, createRouteController);
-router.put("/update-busroute/:routeId", isAuth, isAdmin, updateRouteController)          
+router.put("/update-busroute/:routeId", isAuth, isAdmin, updateRouteController)   
+router.get("/route-detail/:routeId", isAuth, isAdmin, getBusRouteByIdController)       
 
 
 export default router;
